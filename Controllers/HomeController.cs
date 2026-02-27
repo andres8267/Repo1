1e1d30653c3d7b1f8ed4d043d97d1f93b420112d
Asus <Asus@DESKTOP-U8U7EQU>
2026-02-27 16:10:38 -0500

    Update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -20,11 +20,48 @@ namespace Restaurante2.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel 
+            { 
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier 
+            });
         }
 
+        // GET
         public IActionResult Restaurante()
         {
+            var menu = MenuService.Instancia.ObtenerMenu();
+            return View(menu);
+        }
+
+        // POST
+        [HttpPost]
+        public IActionResult Restaurante(
+            int Hamburguesa,
+            int Cerveza,
+            int Gaseosa,
+            int Ensalada,
+            int Salchichas,
+            int Refresco,
+            int Sopa,
+            int Postre)
+        {
+            double venta =
+                Hamburguesa * 5 +
+                Cerveza * 3 +
+                Gaseosa * 2.5 +
+                Ensalada * 4 +
+                Salchichas * 3.5 +
+                Refresco * 2 +
+                Sopa * 3 +
+                Postre * 4.5;
+
+            double impuesto = venta * 0.15;
+            double total = venta + impuesto;
+
+            ViewBag.Venta = venta.ToString("F2");
+            ViewBag.Impuesto = impuesto.ToString("F2");
+            ViewBag.Total = total.ToString("F2");
+
             var menu = MenuService.Instancia.ObtenerMenu();
             return View(menu);
         }
